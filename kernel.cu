d98ae5d89bcfbc6cd622a035f5c73f3fa038aa64
Filip Karnis <karnisfilip@gmail.com>
2022-11-15 21:01:11 +0100

    shared memmory

--- kernel.cu
+++ kernel.cu
@@ -1,21 +1,49 @@
+#define BLOCK_DIM 22
 #define BLOCK_SIZE 484
 
 __global__ void solve(sGalaxy A, sGalaxy B, float* distances, int n) {
 //    printf("GRID: %d %d\n", gridDim.x, gridDim.y);
     int bid = threadIdx.y * blockDim.x + threadIdx.x;
     int i = blockDim.x * blockIdx.x + threadIdx.x;
+    int ii = blockDim.y * blockIdx.y + threadIdx.x;
     int j = blockDim.y * blockIdx.y + threadIdx.y;
 
     __shared__ float D[BLOCK_SIZE];
     D[bid] = 0.0f;
 
+    __shared__ float As[BLOCK_DIM][3][2];
+    __shared__ float Bs[BLOCK_DIM][3][2];
+
+    if (threadIdx.y == 0) {
+        As[threadIdx.x][0][0] = A.x[i];
+        As[threadIdx.x][1][0] = A.y[i];
+        As[threadIdx.x][2][0] = A.z[i];
+    } else if (threadIdx.y == 1) {
+        As[threadIdx.x][0][1] = A.x[ii];
+        As[threadIdx.x][1][1] = A.y[ii];
+        As[threadIdx.x][2][1] = A.z[ii];
+    } else if (threadIdx.y == 2) {
+        Bs[threadIdx.x][0][0] = B.x[i];
+        Bs[threadIdx.x][1][0] = B.y[i];
+        Bs[threadIdx.x][2][0] = B.z[i];
+    } else if (threadIdx.y == 3) {
+        Bs[threadIdx.x][0][1] = B.x[ii];
+        Bs[threadIdx.x][1][1] = B.y[ii];
+        Bs[threadIdx.x][2][1] = B.z[ii];
+    }
+    __syncthreads();
+
     if (i < n && j < n && i < j) {
-        float da = sqrt((A.x[i]-A.x[j])*(A.x[i]-A.x[j])
-                        + (A.y[i]-A.y[j])*(A.y[i]-A.y[j])
-                        + (A.z[i]-A.z[j])*(A.z[i]-A.z[j]));
-        float db = sqrt((B.x[i]-B.x[j])*(B.x[i]-B.x[j])
-                        + (B.y[i]-B.y[j])*(B.y[i]-B.y[j])
-                        + (B.z[i]-B.z[j])*(B.z[i]-B.z[j]));
+        float da = sqrt(
+                (As[threadIdx.x][0][0]-As[threadIdx.y][0][1])*(As[threadIdx.x][0][0]-As[threadIdx.y][0][1])
+                + (As[threadIdx.x][1][0]-As[threadIdx.y][1][1])*(As[threadIdx.x][1][0]-As[threadIdx.y][1][1])
+                + (As[threadIdx.x][2][0]-As[threadIdx.y][2][1])*(As[threadIdx.x][2][0]-As[threadIdx.y][2][1])
+                );
+        float db = sqrt(
+                (Bs[threadIdx.x][0][0]-Bs[threadIdx.y][0][1])*(Bs[threadIdx.x][0][0]-Bs[threadIdx.y][0][1])
+                + (Bs[threadIdx.x][1][0]-Bs[threadIdx.y][1][1])*(Bs[threadIdx.x][1][0]-Bs[threadIdx.y][1][1])
+                + (Bs[threadIdx.x][2][0]-Bs[threadIdx.y][2][1])*(Bs[threadIdx.x][2][0]-Bs[threadIdx.y][2][1])
+        );
         D[bid] = (da-db) * (da-db);
 //        printf("(%d %d) D[%d] = %f\n", i, j, bid, D[bid]);
     }

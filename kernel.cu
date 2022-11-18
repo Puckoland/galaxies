fb09002c522594c7d0e8b06e1ffb0a8a9a932f14
Filip Karnis <karnisfilip@gmail.com>
2022-11-19 00:53:00 +0100

    almost

--- kernel.cu
+++ kernel.cu
@@ -1,54 +1,86 @@
-#define BLOCK_DIM 22
+#define BLOCK_DIM 2
 #define BLOCK_SIZE BLOCK_DIM * BLOCK_DIM
 
-__global__ void solve(sGalaxy A, sGalaxy B, float* distances, int n) {
+__global__ void solve(sGalaxy A, sGalaxy B, float* distances, int n, int per_thread) {
 //    printf("GRID: %d %d\n", gridDim.x, gridDim.y);
+//    printf("PER: %d\n", per_thread);
+    int base = BLOCK_SIZE * blockIdx.x;
     int bid = threadIdx.y * blockDim.x + threadIdx.x;
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    int ii = blockDim.y * blockIdx.y + threadIdx.x;
-    int j = blockDim.y * blockIdx.y + threadIdx.y;
+    int i = base + bid;
 
     __shared__ float D[BLOCK_SIZE];
     D[bid] = 0.0f;
 
-    __shared__ float As[BLOCK_DIM][3][2];
-    __shared__ float Bs[BLOCK_DIM][3][2];
+    __shared__ float As[BLOCK_SIZE * 3];
+    __shared__ float Bs[BLOCK_SIZE * 3];
 
-    if (threadIdx.y == 0) {
-        As[threadIdx.x][0][0] = A.x[i];
-        As[threadIdx.x][1][0] = A.y[i];
-        As[threadIdx.x][2][0] = A.z[i];
-    } else if (threadIdx.y == 1) {
-        As[threadIdx.x][0][1] = A.x[ii];
-        As[threadIdx.x][1][1] = A.y[ii];
-        As[threadIdx.x][2][1] = A.z[ii];
-    } else if (threadIdx.y == 2) {
-        Bs[threadIdx.x][0][0] = B.x[i];
-        Bs[threadIdx.x][1][0] = B.y[i];
-        Bs[threadIdx.x][2][0] = B.z[i];
-    } else if (threadIdx.y == 3) {
-        Bs[threadIdx.x][0][1] = B.x[ii];
-        Bs[threadIdx.x][1][1] = B.y[ii];
-        Bs[threadIdx.x][2][1] = B.z[ii];
+    // LOAD SHARED
+    if (bid < n) {
+        As[bid*3] = A.x[i];
+        As[bid*3 + 1] = A.y[i];
+        As[bid*3 + 2] = A.z[i];
+
+        Bs[bid*3] = B.x[i];
+        Bs[bid*3 + 1] = B.y[i];
+        Bs[bid*3 + 2] = B.z[i];
     }
     __syncthreads();
 
-    if (i < n && j < n && i > j) {
+    // PRINT SHARED MEMORY
+//    if (bid == 0 && blockIdx.x == 0) {
+//        printf("A:\n");
+//        printf("%f %f %f\n", A.x[0], A.y[0], A.z[0]);
+//        printf("%f %f %f\n", A.x[1], A.y[1], A.z[1]);
+//        printf("%f %f %f\n", A.x[2], A.y[2], A.z[2]);
+//        printf("%f %f %f\n", A.x[3], A.y[3], A.z[3]);
+//        printf("As:\n");
+//        printf("%f %f %f\n", As[0], As[1], As[2]);
+//        printf("%f %f %f\n", As[3], As[4], As[5]);
+//        printf("%f %f %f\n", As[6], As[7], As[8]);
+//        printf("%f %f %f\n", As[9], As[10], As[11]);
+//    }
+//    if (bid == 0 && blockIdx.x == 1) {
+//        printf("A:\n");
+//        printf("%f %f %f\n", A.x[4], A.y[4], A.z[4]);
+//        printf("As:\n");
+//        printf("%f %f %f\n", As[0], As[1], As[2]);
+//    }
+
+    // COMPUTE
+    float tmp = 0.0f;
+    for (int k = bid + 1; k < BLOCK_SIZE && base + k < n; k++) {
+        printf("%d %d\n", i, k);
+
+        float Ax = A.x[i];
+        float Ay = A.y[i];
+        float Az = A.z[i];
+        float Bx = B.x[i];
+        float By = B.y[i];
+        float Bz = B.z[i];
+
+        if (blockIdx.x == 1) {
+            printf("%f %f %f\n", Ax, Ay, Az);
+            printf("%f %f %f\n", A.x[i], A.y[i], A.z[i]);
+        }
+
         float da = sqrt(
-                (As[threadIdx.x][0][0]-As[threadIdx.y][0][1])*(As[threadIdx.x][0][0]-As[threadIdx.y][0][1])
-                + (As[threadIdx.x][1][0]-As[threadIdx.y][1][1])*(As[threadIdx.x][1][0]-As[threadIdx.y][1][1])
-                + (As[threadIdx.x][2][0]-As[threadIdx.y][2][1])*(As[threadIdx.x][2][0]-As[threadIdx.y][2][1])
-                );
-        float db = sqrt(
-                (Bs[threadIdx.x][0][0]-Bs[threadIdx.y][0][1])*(Bs[threadIdx.x][0][0]-Bs[threadIdx.y][0][1])
-                + (Bs[threadIdx.x][1][0]-Bs[threadIdx.y][1][1])*(Bs[threadIdx.x][1][0]-Bs[threadIdx.y][1][1])
-                + (Bs[threadIdx.x][2][0]-Bs[threadIdx.y][2][1])*(Bs[threadIdx.x][2][0]-Bs[threadIdx.y][2][1])
+                (Ax - As[k * 3]) * (Ax - As[k * 3]) +
+                (Ay - As[k * 3 + 1]) * (Ay - As[k * 3 + 1]) +
+                (Az - As[k * 3 + 2]) * (Az - As[k * 3 + 2])
         );
-        D[bid] = (da-db) * (da-db);
-//        printf("(%d %d) D[%d] = %f\n", i, j, bid, D[bid]);
+        float db = sqrt(
+                (Bx - Bs[k * 3]) * (Bx - Bs[k * 3]) +
+                (By - Bs[k * 3 + 1]) * (By - Bs[k * 3 + 1]) +
+                (Bz - Bs[k * 3 + 2]) * (Bz - Bs[k * 3 + 2])
+        );
+        tmp += (da-db) * (da-db);
+//        printf("%d = %f\n", i, tmp);
+//        printf("(%d %d) D[%d] = %f\n", i, k, bid, tmp);
     }
-
+    D[bid] = tmp;
     __syncthreads();
+
+    // REDUCE
     if (bid == 0) {
         float sum = 0.0f;
         for (int o = 0; o < BLOCK_SIZE; o++) {
@@ -67,8 +99,9 @@ int roundUp(int value, int div) {
 }
 
 float solveGPU(sGalaxy A, sGalaxy B, int n) {
-    int a = roundUp(n, BLOCK_DIM);
-    int b = roundUp(a, 1);
+    int per_thread = roundUp(n, BLOCK_SIZE);
+    int a = roundUp(n, BLOCK_SIZE);
+    int b = 1;
     dim3 dimGrid (a, b);
     dim3 dimBlock (BLOCK_DIM, BLOCK_DIM);
     float* distances;
@@ -81,7 +114,7 @@ float solveGPU(sGalaxy A, sGalaxy B, int n) {
         exit(1);
     }
 
-    solve<<<dimGrid, dimBlock>>>(A, B, distances, n);
+    solve<<<dimGrid, dimBlock>>>(A, B, distances, n, per_thread);
     cudaMemcpy(dist, distances, size, cudaMemcpyDeviceToHost);
 
     float result = sqrt(1/((float)n*((float)n-1)) * *dist);

be718f4342065111c159509af8fc88bfe26f8382
Filip Karnis <karnisfilip@gmail.com>
2022-11-19 02:12:45 +0100

    loop

--- kernel.cu
+++ kernel.cu
@@ -11,73 +11,78 @@ __global__ void solve(sGalaxy A, sGalaxy B, float* distances, int n, int per_thr
     __shared__ float D[BLOCK_SIZE];
     D[bid] = 0.0f;
 
-    __shared__ float As[BLOCK_SIZE * 3];
-    __shared__ float Bs[BLOCK_SIZE * 3];
+    // TODO: load and compute in loops
+    for (int t = 0; t < per_thread; t++) {
+        int index = t * BLOCK_SIZE + i;
 
-    // LOAD SHARED
-    if (bid < n) {
-        As[bid*3] = A.x[i];
-        As[bid*3 + 1] = A.y[i];
-        As[bid*3 + 2] = A.z[i];
+        __shared__ float As[BLOCK_SIZE * 3];
+        __shared__ float Bs[BLOCK_SIZE * 3];
 
-        Bs[bid*3] = B.x[i];
-        Bs[bid*3 + 1] = B.y[i];
-        Bs[bid*3 + 2] = B.z[i];
-    }
-    __syncthreads();
+        // LOAD SHARED
+        if (bid < n) {
+            As[bid * 3] = A.x[index];
+            As[bid * 3 + 1] = A.y[index];
+            As[bid * 3 + 2] = A.z[index];
 
-    // PRINT SHARED MEMORY
-//    if (bid == 0 && blockIdx.x == 0) {
-//        printf("A:\n");
-//        printf("%f %f %f\n", A.x[0], A.y[0], A.z[0]);
-//        printf("%f %f %f\n", A.x[1], A.y[1], A.z[1]);
-//        printf("%f %f %f\n", A.x[2], A.y[2], A.z[2]);
-//        printf("%f %f %f\n", A.x[3], A.y[3], A.z[3]);
-//        printf("As:\n");
-//        printf("%f %f %f\n", As[0], As[1], As[2]);
-//        printf("%f %f %f\n", As[3], As[4], As[5]);
-//        printf("%f %f %f\n", As[6], As[7], As[8]);
-//        printf("%f %f %f\n", As[9], As[10], As[11]);
-//    }
-//    if (bid == 0 && blockIdx.x == 1) {
-//        printf("A:\n");
-//        printf("%f %f %f\n", A.x[4], A.y[4], A.z[4]);
-//        printf("As:\n");
-//        printf("%f %f %f\n", As[0], As[1], As[2]);
-//    }
-
-    // COMPUTE
-    float tmp = 0.0f;
-    for (int k = bid + 1; k < BLOCK_SIZE && base + k < n; k++) {
-        printf("%d %d\n", i, k);
-
-        float Ax = A.x[i];
-        float Ay = A.y[i];
-        float Az = A.z[i];
-        float Bx = B.x[i];
-        float By = B.y[i];
-        float Bz = B.z[i];
-
-        if (blockIdx.x == 1) {
-            printf("%f %f %f\n", Ax, Ay, Az);
-            printf("%f %f %f\n", A.x[i], A.y[i], A.z[i]);
+            Bs[bid * 3] = B.x[index];
+            Bs[bid * 3 + 1] = B.y[index];
+            Bs[bid * 3 + 2] = B.z[index];
         }
+        __syncthreads();
 
-        float da = sqrt(
-                (Ax - As[k * 3]) * (Ax - As[k * 3]) +
-                (Ay - As[k * 3 + 1]) * (Ay - As[k * 3 + 1]) +
-                (Az - As[k * 3 + 2]) * (Az - As[k * 3 + 2])
-        );
-        float db = sqrt(
-                (Bx - Bs[k * 3]) * (Bx - Bs[k * 3]) +
-                (By - Bs[k * 3 + 1]) * (By - Bs[k * 3 + 1]) +
-                (Bz - Bs[k * 3 + 2]) * (Bz - Bs[k * 3 + 2])
-        );
-        tmp += (da-db) * (da-db);
-//        printf("%d = %f\n", i, tmp);
-//        printf("(%d %d) D[%d] = %f\n", i, k, bid, tmp);
+        // PRINT SHARED MEMORY
+//        if (bid == 0 && blockIdx.x == 0) {
+//            printf("A:\n");
+//            printf("%f %f %f\n", A.x[0], A.y[0], A.z[0]);
+//            printf("%f %f %f\n", A.x[1], A.y[1], A.z[1]);
+//            printf("%f %f %f\n", A.x[2], A.y[2], A.z[2]);
+//            printf("%f %f %f\n", A.x[3], A.y[3], A.z[3]);
+//            printf("As:\n");
+//            printf("%f %f %f\n", As[0], As[1], As[2]);
+//            printf("%f %f %f\n", As[3], As[4], As[5]);
+//            printf("%f %f %f\n", As[6], As[7], As[8]);
+//            printf("%f %f %f\n", As[9], As[10], As[11]);
+//        }
+//        if (bid == 0 && blockIdx.x == 1) {
+//            printf("A:\n");
+//            printf("%f %f %f\n", A.x[4], A.y[4], A.z[4]);
+//            printf("As:\n");
+//            printf("%f %f %f\n", As[0], As[1], As[2]);
+//        }
+
+        // COMPUTE
+        float tmp = 0.0f;
+        for (int k = bid + 1; k < BLOCK_SIZE && base + k < n; k++) {
+            printf("%d %d\n", i, k);
+
+            float Ax = A.x[i];
+            float Ay = A.y[i];
+            float Az = A.z[i];
+            float Bx = B.x[i];
+            float By = B.y[i];
+            float Bz = B.z[i];
+
+            if (blockIdx.x == 1) {
+                printf("%f %f %f\n", Ax, Ay, Az);
+                printf("%f %f %f\n", A.x[i], A.y[i], A.z[i]);
+            }
+
+            float da = sqrt(
+                    (Ax - As[k * 3]) * (Ax - As[k * 3]) +
+                    (Ay - As[k * 3 + 1]) * (Ay - As[k * 3 + 1]) +
+                    (Az - As[k * 3 + 2]) * (Az - As[k * 3 + 2])
+            );
+            float db = sqrt(
+                    (Bx - Bs[k * 3]) * (Bx - Bs[k * 3]) +
+                    (By - Bs[k * 3 + 1]) * (By - Bs[k * 3 + 1]) +
+                    (Bz - Bs[k * 3 + 2]) * (Bz - Bs[k * 3 + 2])
+            );
+            tmp += (da - db) * (da - db);
+            //        printf("%d = %f\n", i, tmp);
+            //        printf("(%d %d) D[%d] = %f\n", i, k, bid, tmp);
+        }
+        D[bid] += tmp;
     }
-    D[bid] = tmp;
     __syncthreads();
 
     // REDUCE
